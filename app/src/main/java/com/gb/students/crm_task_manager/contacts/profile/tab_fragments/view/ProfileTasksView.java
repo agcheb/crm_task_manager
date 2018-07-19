d3b41e7392f66043229454921a565a910d5038b5
v4n0v <avetchinov@gmail.com>
2018-07-19 23:37:32 +0300

    11

--- app/src/main/java/com/gb/students/crm_task_manager/contacts/profile/tab_fragments/view/ProfileTasksView.java
+++ app/src/main/java/com/gb/students/crm_task_manager/contacts/profile/tab_fragments/view/ProfileTasksView.java
@@ -4,5 +4,5 @@ package com.gb.students.crm_task_manager.contacts.profile.tab_fragments.view;
  * Created by avetc on 19.07.2018.
  */
 
-public class ProfileTaskView {
+public class ProfileTasksView {
 }
